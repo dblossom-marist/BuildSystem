53f52e960c1c376f61a1756fc10e9a67027cf0d2
Dan Blossom <daniel.blossom1@marist.edu>
2019-04-27 19:43:47 -0400

    added post install script and other enhancements for build system

--- post_install.sh
+++ post_install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # put the service file in /lib/systemd/system/
-mv /usr/share/perfmon/perfmon.service /lib/systemd/system
+echo "executing post install steps.....";
+mv /usr/share/perfmon/perfmon.service /lib/systemd/system;
+# Symlink an 'executable' to the actual python script.
+ln -s /usr/share/perfmon/MetricsCollector.py /usr/bin/PerfMonMetricsCollector;
 # enable said service
-systemctl enable perfmon.service
+systemctl enable perfmon.service;
 # Reload
-systemctl daemon-reload
+systemctl daemon-reload;
 # start said service
-systemctl start perfmon.service
+systemctl start perfmon.service;
